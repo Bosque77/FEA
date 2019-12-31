f09f22d20b421da6142e0fab64da14dd8538e891
Bosque77 <forestschwrtz@gmail.com>
2019-12-31 13:19:36 -0500

    Add files via upload

--- modal_analysis.m
+++ modal_analysis.m
@@ -16,10 +16,8 @@ modal_loads = [
 0	0	177712704	0;
 0	0	0	138843296]
 
-%%
-[eigenVectors,eigenValues] = eig(K,M)
 
-[r,c] = size(M)
+[r,c] = size(M);
 
 % Solution 1
 syms l
@@ -31,6 +29,59 @@ for j=1:length(gamma)
     u(:,j) = null(K-gamma(j).*M);  %eigenvectors
 end
 
+
+Mg = u'*M*u;
+Kg = u'*K*u;
+Fg = u'*F;
+
+% index=1;
+% syms t
+% for w=(2*pi):100*2*pi:10001*2*pi
+%    A = (-w^2.*Mg+Kg);
+%    b = Fg;
+%    Z = A\b;
+%    mode_shape_factors(:,index) = Z;
+%    x_test(:,index) = u*Z;
+%    x(:,index) = u*Z*exp(1i*w);
+%    beam_loads(:,index) = modal_loads*Z;
+% %    mag = abs(x)
+% %    phase = angle(x)
+%    index = index+1;
+% end
+
+index=1;
+syms t
+
+% Adjust these variables to set the modal frequency
+alpha = 0.01;
+beta = 0.01;
+
+C = alpha*Mg + beta*Kg;
+
+[r,c] = size(Mg);
+C = eye(r)*0.04
+
+spread = 0.05;
+for w=1324*2*pi:spread:1325*2*pi
+   A = (-w^2.*Mg+i*w*C+Kg);
+   b = Fg;
+   Z = A\b;
+   mode_shape_factors(:,index) = Z;
+   x(:,index) = abs(u*Z*exp(i*w));
+   x1(:,index) = u*Z;
+   beam_loads(:,index) = abs(modal_loads*Z);
+   mag = abs(x)
+   phase = angle(x);
+   index = index+1;
+end
+
+
+T2 = x(2,:);
+indep_var = 1320*2*pi:spread:1350*2*pi
+plot(indep_var,T2)
+
+indep_var = indep_var./(2*pi);
+plot(indep_var,beam_loads(1,:))
 
 
 % 
@@ -44,10 +95,7 @@ end
 % %     u(:,j) = u(:,j)/u(1,j);  %eigenvectors
 % % end
 %  
-% Mg = u'*M*u
-% Kg = u'*K*u
-% Fg = u'*F
-% 
+
 % 
 % index=1;
 % syms t
@@ -82,6 +130,5 @@ end
 %    subplot(2,1,2)
 %    plot(input,mag(2,:))
 % %    plot(input,beam_loads(1,:))
-%    
-%    
+% 
 
